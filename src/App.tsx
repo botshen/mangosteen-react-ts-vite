82044dc8c622dca3af002b7e3affaecce89ce343
shenxin <wb_shenxin@meituan.com>
2022-07-14 19:48:31 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Hello World" />
+      <Header title="Hello World" />
+    </div>
+  );
+}
+
+export default App;
