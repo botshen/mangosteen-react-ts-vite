554300bb0f8c0a8070d55503a00cec8deed92cb3
shenxin <wb_shenxin@meituan.com>
2022-07-17 00:24:29 +0800

    style: 玩了一下 tailwindcss

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import s from "./Welcome.module.scss";
+import logo from "../assets/icons/mangosteen.svg";
 
 export const Welcome: React.FC = () => {
   return (
     <div className={s.wrapper}>
-      <header className={s.welcome}>
-        <h1>山竹记账</h1>
+      <header>
+        <img src={logo} alt="logo" />
+        <h1 className="text-2xl">山竹记账</h1>
       </header>
       <main><Outlet /></main>
-      <footer> butttons</footer>
     </div>
   );
 };
